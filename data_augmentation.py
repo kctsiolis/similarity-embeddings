522f51e80fe8a23a174a2ee571142e828853e001
KC Tsiolis <kc.tsiolis@mail.mcgill.ca>
2021-07-26 12:29:26 -0400

    Adding random crop augmentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -7,7 +7,7 @@ Augmentations supported:
 
 from torchvision import transforms
 import torch
-import numpy as np
+import math
 
 #Gaussian blur with parameter sigma (which controls the blur intensity)
 def gaussian_blur_sigma(data: torch.Tensor, sigma_max: float, 
@@ -45,41 +45,6 @@ def gaussian_blur_sigma(data: torch.Tensor, sigma_max: float,
 
     return data, sim_prob
 
-def gaussian_blur_kernel(data: torch.Tensor, device: torch.device, 
-    sigma: float = 8.0, beta: float = 0.2) -> tuple([torch.Tensor, torch.Tensor]):
-    """Apply gaussian blur with randomly sampled kernel size parameter.
-
-    Kernel size is sampled uniformly i.i.d. (over valid values) for 
-    each image in the data, while the standard deviation is fixed.
-
-    Args:
-        data: The data to be augmented.
-        device: The device to store the data on for augmentation.
-        sigma: The standard deviation of the Gaussian blur.
-        beta: Parameter of the similarity probability.
-
-    Returns:
-        The augmented data and the similarity probabilities
-
-    """
-    img_size = min(data.shape[2], data.shape[3])
-    max_kernel_size = img_size if img_size % 2 == 1 else img_size - 1 
-    #Set the possible kernel sizes (must be odd numbers)
-    kernel_sizes = np.arange(max_kernel_size // 2 + 1) * 2 + 1
-    #Sample kernel sizes (one per image in the batch)
-    alpha = np.random.choice(kernel_sizes, size=(data.shape[0],))
-    alpha = torch.from_numpy(alpha).to(device)
-    for i, image in enumerate(data):
-        data[i,:,:,:] = transforms.GaussianBlur(alpha[i].item(), sigma)(image)
-
-    #Compute similarity probability
-    #Map kernel sizes to [0, kernel_size // 2 + 1]
-    #Add noise in order to get continuity
-    alpha = (alpha - 1) / 2
-    sim_prob = torch.exp(-beta * (alpha + torch.rand(data.shape[0]).to(device) - 0.5))
-
-    return data, sim_prob
-
 def color_jitter(data: torch.Tensor, s_max: float, 
     beta: float, device = torch.device, random: bool = True
     ) -> tuple([torch.Tensor, torch.Tensor]):
@@ -94,6 +59,25 @@ def color_jitter(data: torch.Tensor, s_max: float,
         data[i,:,:,:] = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)(image)
 
     sim_prob = torch.exp(-beta * s)
+
+    return data, sim_prob
+
+def random_crop(data: torch.Tensor, s_max: int,
+    beta: float, device = torch.device, random: bool = True
+    ) -> tuple([torch.Tensor, torch.Tensor]):
+    img_size = min(data.shape[2], data.shape[3])
+    if s_max >= img_size:
+        raise ValueError("Maximum augmentation strength must be at most \
+            one less than image height/width.")
+    if random:
+        s = (math.ceil(torch.rand(data.shape[0])*s_max)).to(device)
+    else:
+        s = (torch.ones(data.shape[0])*s_max).to(device)
+    
+    for i, image in enumerate(data):
+        data[i,:,:,:] = transforms.RandomCrop(img_size-s)(image)
+
+    sim_prob = torch.exp(-beta * (s / img_size))
 
     return data, sim_prob
 
@@ -121,9 +105,9 @@ def augment(data: torch.Tensor, augmentation: str, device: torch.device,
     augmented_data = data.detach().clone()
     if augmentation == 'blur-sigma':
         return gaussian_blur_sigma(augmented_data, alpha_max, beta, device, random)
-    elif augmentation == 'blur-kernel':
-        return gaussian_blur_kernel(augmented_data, device)
     elif augmentation == 'color-jitter':
         return color_jitter(augmented_data, alpha_max, beta, device, random)
+    elif augmentation == 'random_crop':
+        return random_crop(augmented_data, alpha_max, beta, device, random)
     else:
         raise NotImplementedError
